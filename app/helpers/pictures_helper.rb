3b6a2b2ff7fa91c99d7f17f187f86e712d7d8c92
Esteban Cordero <ccordero@growthaccelerationpartners.com>
2020-11-20 19:23:11 -0600

    Extra stuff

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -1,2 +1,7 @@
 module PicturesHelper
+  def image_detail_modal_link picture
+    link_to([picture.gallery, picture], remote: true ) do
+      concat image_tag picture.image, class: 'card-img rounded mx-auto d-block'
+    end
+  end
 end
